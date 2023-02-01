bf51fb024aaf42607c16adc8414b3b8eaf5c8c87
shaw <1939484341@qq.com>
2023-02-01 17:09:20 +0800

    添加更多虚拟形象

--- PluginModules/VirtualCharacterPlugin/Resources/html/src/autoload.js
+++ PluginModules/VirtualCharacterPlugin/Resources/html/src/autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/",
 			tools: ["switch-model", "switch-texture"]
 		});
 	});
